4357652bac80a91cd232fa91fabe27bdc98103eb
tidhardavid <tidhardavid@users.noreply.github.com>
2017-04-25 22:12:50 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,5 @@
 <?php
 
-// Page créé par Shepard [Fabian Pijcke] <Shepard8@laposte.net>
-// Arno Esterhuizen <arno.esterhuizen@gmail.com>
-// et Romain Bourdon <rromain@romainbourdon.com>
-// et Hervé Leclerc <herve.leclerc@alterway.fr>
-//  
-// Mise  �our par Herve Leclerc herve.leclerc@alterway.fr
-// Ic󭤳 par Mark James <http://www.famfamfam.com/lab/icons/silk/>
-
-
-
 //chemin jusqu'au fichier de conf de WampServer
 $wampConfFile = '../wampmanager.conf';
 
